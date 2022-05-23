86dc6aecdfb296b806f7ba4bcccd4a08f00871bd
HOURDOU DAVID <davidhourdou@gmail.com>
2022-05-23 16:34:39 +0200

    home page

---
+++ .history/src/Controller/ProgramController_20220523160111.php
@@ -0,0 +1,29 @@
+<?php
+
+// src/Controller/ProgramController.php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+class ProgramController extends AbstractController
+
+{
+
+    public function index(): Response
+
+    {
+
+        return new Response(
+
+            '<html><body>Wild Series Index</body></html>'
+
+        );
+
+    }
+
+}
---
+++ .history/src/Controller/ProgramController_20220523161456.php
@@ -0,0 +1,32 @@
+<?php
+
+// src/Controller/ProgramController.php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProgramController extends AbstractController
+
+{
+
+    
+
+    public function index(): Response
+
+    {
+
+        return new Response(
+
+            '<html><body>Wild Series Index</body></html>'
+
+        );
+
+    }
+
+}
