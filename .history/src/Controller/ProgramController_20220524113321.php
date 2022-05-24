1076a7ef6b88419126f873daf3879d8ea9e1da53
HOURDOU DAVID <davidhourdou@gmail.com>
2022-05-24 12:03:55 +0200

    route show

---
+++ .history/src/Controller/ProgramController_20220524113321.php
@@ -0,0 +1,30 @@
+<?php
+
+// src/Controller/ProgramController.php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProgramController extends AbstractController
+
+{
+    #[Route('/program/', name: 'program_index')]
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
+         ]);
+    }
+
+    #[Route('/program/{id}', methods: ['GET'], name: 'program_show')]
+    public function show(): Response
+    {
+        return $this->render('program/show.html.twig', []);
+    }
+}
